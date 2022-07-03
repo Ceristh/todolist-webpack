5ab6924baf394e81f4644564a252625e4f32ae65
ceristh <ceristh@gmail.com>
2022-07-03 15:34:42 -0400

    Agrego Todo Class

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-import { saludar } from "./js/componentes.js";
 import './styles.css';
+import { Todo } from './classes/todo.class.js';
 
-const nombre = 'Ceristh';
+const tarea = new Todo('Aprender Javascript');
 
-saludar(nombre);
+console.log(tarea);

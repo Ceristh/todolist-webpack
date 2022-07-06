c9dc9b57028dad2045ae0d60fdae1c86ed27afa0
ceristh <ceristh@gmail.com>
2022-07-06 15:05:30 -0400

    Guardando y recuperando Todos

--- src/index.js
+++ src/index.js
@@ -1,19 +1,7 @@
 import './styles.css';
 import { Todo, TodoList } from './classes';
 import { crearTodoHtml } from './js/componentes.js'
-// import { Todo } from './classes/todo.class.js';
-// import { TodoList } from './classes/todo-list.class.js';
 
 export const todoList = new TodoList();
 
-// const tarea = new Todo('Aprender Javascript!!');
-
-// todoList.nuevoTodo( tarea );
-
-// tarea.completado = false;
-
-// console.log( todoList );
-
-// crearTodoHtml( tarea );
-
-// localStorage.setItem('mi-key', 'ABC1234');
+todoList.todos.forEach( todo => crearTodoHtml( todo ) );
